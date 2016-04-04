460f0705814d6e05c9e0e48c208f9711d3befcec
Andreas Søvik <andreas.sovik@vg.no>
2016-04-04 10:12:50 +0200

    Make example use assets-images plugin

--- examples/client-server/client.js
+++ examples/client-server/client.js
@@ -1,4 +1,10 @@
+import rocjs from './files/rocjs.png';
+
 function start() {
+    var img = document.createElement('img');
+    img.src = rocjs;
+
+    document.getElementById('roc').appendChild(img);
     window.alert('Hello World');
 }
 
